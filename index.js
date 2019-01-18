cf0ac1f54351e26f0b0201d3bf8ccdcd4c01b2b6
Carla Stickler <carlastickler@gmail.com>
2019-01-18 01:53:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+const startingPoint = 42;
+const feetInBlock = 264;
+// 43 -> 1 
+// 50 -> 8 
+// 34 -> 8
+function distanceFromHqInBlocks(block) {
+  if (block < startingPoint) {
+    return startingPoint - block;
+  } else {
+    return block - startingPoint;
+  }
+}
+
+function distanceFromHqInFeet(block) {
+  if (block < startingPoint) {
+    return (startingPoint - block) * feetInBlock;
+  } else {
+    return (block - startingPoint) * feetInBlock;
+  }
+}
+
+function distanceTravelledInFeet(startingBlock, endingBlock) {
+   if (endingBlock < startingBlock) {
+    return (startingBlock - endingBlock) * feetInBlock;
+  } else {
+    return (endingBlock - startingBlock) * feetInBlock;
+  }
+}
+
+function calculatesFarePrice(startingBlock, endingBlock) {
+  const feetTraveled = distanceTravelledInFeet(startingBlock, endingBlock)
+  if (feetTraveled <= 400) {
+    return 0
+  } else if (400 < feetTraveled && feetTraveled < 2000) {
+    return (feetTraveled - 400) * 0.02  
+  } else if (2000 < feetTraveled && feetTraveled < 2500) {
+    return 25
+  } else if (feetTraveled > 2500) {
+    return "cannot travel that far"
+  }
+}
