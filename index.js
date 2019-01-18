2748074b3b16bf253e794b9ff2f5d13304530ff0
Carla Stickler <carlastickler@gmail.com>
2019-01-18 01:22:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+const startingPoint = 42
+// 43 -> 1 
+// 50 -> 8 
+// 34 -> 8
+function distanceFromHqInBlocks(block) {
+  return block - startingPoint
+  if (block < startingPoint) {
+    return startingPoint - block
+  }
+}
