5d0dfc7ac900d314db55113519aacc3e97dcd3ed
Carla Stickler <carlastickler@gmail.com>
2019-01-18 01:28:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 // Code your solution in this file!
-const startingPoint = 42
+const startingPoint = 42;
+const feetInBlock = 264;
 // 43 -> 1 
 // 50 -> 8 
 // 34 -> 8
 function distanceFromHqInBlocks(block) {
   if (block < startingPoint) {
-    return startingPoint - block
+    return startingPoint - block;
   } else {
-    return block - startingPoint
+    return block - startingPoint;
+  }
+}
+
+function distanceFromHqInFeet(block) {
+  if (block < startingPoint) {
+    return (startingPoint - block) * feetInBlock;
+  } else {
+    return (block - startingPoint) * feetInBlock;
   }
 }
